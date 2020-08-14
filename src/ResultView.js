e9fbb442cf7b0741911e6cd8136599de2e5884f9
frauzufall <mail@frauzufall.de>
2020-08-14 17:59:06 +0200

    Support markdown rendering via showdown

--- src/ResultView.js
+++ src/ResultView.js
@@ -1,4 +1,6 @@
 import React from "react";
+var showdown  = require('showdown'),
+    converter = new showdown.Converter();
 
 function printTime(time) {
     var date = new Date(time.raw).toString();
@@ -9,7 +11,7 @@ export default ({ result }) => (
   <li className="sui-result dense">
   <a href={result.gitterid.raw}>
   <span class="sui-result-header"><span class="author">{result.display_name.raw}</span> ({result.username.raw}) | {result.group_name.raw} | {printTime(result.sent)}</span>
-  <span class="sui-result-body">{result.message.raw}</span>
+  <span class="sui-result-body" dangerouslySetInnerHTML={{ __html: converter.makeHtml(result.message.raw) }} ></span>
   </a>
   </li>
 );
